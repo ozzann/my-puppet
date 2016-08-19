4693adf90db3719b5bba40350080c616235c5200
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-19 15:02:50 +0100

    fix docker build command

--- modules/production/manifests/init.pp
+++ modules/production/manifests/init.pp
@@ -4,7 +4,7 @@ class production {
     }
 
     exec {'docker -d -p 9000:9000 app':
-        onlyif => 'docker build app .',
+        onlyif => 'docker build -t app .',
         cwd    => '/home/vagrant/app',
         path   => '/var/run/docker:/var/lib/docker:/usr/bin',
     }
