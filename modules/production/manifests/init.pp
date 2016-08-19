ab88600531839e1f548a4047cd87cdbe48a7aeb7
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-19 16:30:33 +0100

    fix using variable

--- modules/production/manifests/init.pp
+++ modules/production/manifests/init.pp
@@ -6,33 +6,33 @@ class production {
     }
 
     file {'/home/vagrant/app/deploy.sh':
-        mode => 0755,
+        mode    => 0755,
         require => Vcsrepo["/home/vagrant/app"],
     }
 
-    $docker path='/usr/bin'
+    $docker path="/usr/bin"
     exec {'stop_all_containers':
         command => 'docker stop $(docker ps -q -a)',
-        path => "$docker_path",
+        path    => "${docker_path}",
     }
     
     exec {'remove_all_containers':
         command => 'docker rm $(docker ps -q -a)', 
-        path => "$docker_path",
-        onlyif => 'stop_all_containers',
+        path    => "${docker_path}",
+        onlyif  => 'stop_all_containers',
     }
 
     exec {'build_app_image':
          command => 'docker build -t app .',
          cwd     => '/ome/vagrant/app',
-         path    => "$docker_path",
+         path    => "${docker_path}",
          timeout => 500,
          onlyif  => 'remove_all_containers',
     }
 
     exec {'run_container':
          command => 'docker run -d -p 9000:9000 app',
-         path    => "$docker_path",
+         path    => "${docker_path}",
          onlyif  => 'build_app_image',
     }
 }
