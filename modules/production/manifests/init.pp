08b23f03390460b4c5e5bc9d44d2508837e22e50
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-19 10:40:22 +0100

    change key

--- modules/production/manifests/init.pp
+++ modules/production/manifests/init.pp
@@ -8,6 +8,6 @@ class production {
     ssh_authorized_key { 'prod_ssh':
         user => 'prod',
         type => 'rsa',
-        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDkuyfYEt2ROOf+kCc1r0f6Fv0Rfp1WyxnBY824r4CA1CvsignwnaLQC86dtZr5slcVchFc9bHdddVPFuYri8tFysxRc6yQo76ha4+4wadpWNU6X6aaxWUReUSXCRiQ9gh2I1CohkvjVehvul+6Qs30Fj7Su00tzbQHqw6ZL5C8DelaVxi8kqvpHMWKB/MtlKtwLhku56ILo9e3DBa22tuWweyVTttkXHAFNszQtg7iLb6cMFs10apCMtQrfjkhEat9G/sQdYIuGVFR33KPv1BSGsj1KQNXv/0fHXXt9QSu4KJ8hHPuqd7OnojbIrTS8a87EsjiFa/ic79xfkOueqouHO0SwTR8DbzA4UuS69qs0dvw/PQSozj6eE0X9vRc8ixHb5u2tOltn7zOI0rKhuFyydEshpQID2WyxncGUHW5TDzwKKj4QFE04DwKOkfMqL1BJ4v/19YUZDYocqoZ7eJZNtUULo4Agh/S/ePAWEBSPc/BPaodd7vRFHdvMurnZBJgA6PDO6N89aHr9r243ZDKmygQ/odmXJ1c8dirdfxDX1ZNcWAwStEX8ifxJOt7RTPr7JGadQUKzfXPTzMF7p9mtwYaL0y4yptB3GU3VNn7XOtq/ZXaoCWR2g2wFm13I+0ildH2vbSFF2mnF+HYcMxRAyg0UYrIGh1t2yN1cidKCQ==',
+        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDIaYRnJsO2mDJ2mrhiK9lxYVssEqqf5DoiyCTDYoCrEwWwkkybMQMeeDTxvc3ufUGf3XwyssfCQex6hjjU7AI5GFv/YJJNJsh4ZtoVyqkTM+V0HzUEWoHJXQ3ByUz3o48uiuxrDmSYY2+KkHEsvQ9und30wmmfq5qxO08wn1yEyd+aZKJMttHnNVG62mu9Lvxja0NG0zG4S4LFFr4oGQrPqCLADBuuN37Ismc61XDLMAoN3Yii7RNcbNRAhcNBwos+n1ix0QVqMk7jIfp/WLrKDZWLly9i7kARZ/E4HujVXIBpIpGMfH25S+RbXA5EBl5fvrbA9iQad9/1Os8IgUPz',
    }
 }
