5875bd6f41f4d9d507e325aa39d017fce6bc9903
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-19 11:37:57 +0100

    add production class

--- modules/production/manifests/init.pp
+++ modules/production/manifests/init.pp
@@ -1,5 +1,7 @@
-vcsrepo { '/home/vagrant/app':
-  ensure   => present,
-  provider => git,
-  source   => 'http://github.com/ozzann/basic-ping-server',
+clas production{
+    vcsrepo { '/home/vagrant/app':
+        ensure   => present,
+        provider => git,
+        source   => 'http://github.com/ozzann/basic-ping-server',
+    }
 }
