b048d4c795ebe3c87b1d96856d5eacdb25ca9998
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-19 16:41:31 +0100

    simplify and call one script

--- modules/production/manifests/init.pp
+++ modules/production/manifests/init.pp
@@ -9,30 +9,9 @@ class production {
         mode    => 0755,
         require => Vcsrepo["/home/vagrant/app"],
     }
-
-    $docker_path="/usr/bin"
-    exec {'stop_all_containers':
-        command => 'docker stop $(docker ps -q -a)',
-        path    => "${docker_path}",
-    }
     
-    exec {'remove_all_containers':
-        command => 'docker rm $(docker ps -q -a)', 
-        path    => "${docker_path}",
-        require => Exec["stop_all_containers"],
-    }
-
-    exec {'build_app_image':
-         command => 'docker build -t app .',
-         cwd     => '/home/vagrant/app',
-         path    => "${docker_path}",
-         timeout => 500,
-         require => Exec["remove_all_containers"],
-    }
-
-    exec {'run_container':
-         command => 'docker run -d -p 9000:9000 app',
-         path    => "${docker_path}",
-         require => Exec["build_app_image"],
+    exec { 'deploy_app':
+       command => "/bin/bash -c '/home/vagrant/app/deploy.sh'",
+       require => File["/home/vagrant/app/deploy.sh"],
     }
 }
