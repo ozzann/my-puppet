d826fc42040b37ab38cdbeafd03d4a6d74d4ce0d
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-09-02 16:52:06 +0100

    docker should be installed before everything else

--- modules/production/manifests/init.pp
+++ modules/production/manifests/init.pp
@@ -1,6 +1,6 @@
 class production {
 
-    include docker
+    require docker
 
     exec {'rm -rf /home/vagrant/app/*':
         path => '/usr/bin:/usr/sbin:/bin',
@@ -14,15 +14,11 @@ class production {
         owner => 'vagrant',
     }
     
-    docker::image { 'mono': 
-        require => File["/home/vagrant/app"],
-    }
-
     exec { 'deploy_app':
        cwd     => "/home/vagrant/app",
        logoutput => 'true',
        command => "/bin/bash -c 'chmod +x deploy.sh; ./deploy.sh'",
-       require => Docker::Image["mono"],
+       require => File["/home/vagrant/app"],
        timeout => 1000,
     }
 }
